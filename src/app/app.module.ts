349c31fb9e2ba4bd94a2e5f98c26e62e0bd21c2b
mmullerx4 <mmullerx4@gmail.com>
2024-12-05 13:01:02 -0500

    w12 fix setup.file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { JournalListComponent } from './journal-list.component';
 import { JournalEditComponent } from './journal-edit.component';
 import { JournalItemComponent } from './journal-item.component';
+import { JournalDetailComponent } from './journal-detail/journal-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JournalListComponent,
     JournalEditComponent,
-    JournalItemComponent
+    JournalItemComponent,
+    JournalDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/journal/journal-detail/journal-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-detail',
+  templateUrl: './journal-detail.component.html',
+  styleUrl: './journal-detail.component.css'
+})
+export class JournalDetailComponent {
+
+}
